cd1261f03b38402d3d6ebcb296f1aabdadfdc5f6
Lennon <jgleydson.8@gmail.com>
2026-05-27 01:30:06 -0300

    Atualizando conexão PostgreSQL

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,17 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-
-  host: process.env.DB_HOST,
-
-  user: process.env.DB_USER,
-
-  database: process.env.DB_NAME,
-
-  password: process.env.DB_PASSWORD,
-
-  port: process.env.DB_PORT,
-
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
